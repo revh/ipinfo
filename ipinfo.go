5aa880f672e9ae6270ac8293ac9e4e2558967b5c
Lorenzo Greco <lorenzo.rev@gmail.com>
2015-06-24 00:11:09 +0200

    Better docs, mock web service response, son tags, use of Decode instead of unmarshall

--- ipinfo.go
+++ ipinfo.go
@@ -1,48 +1,48 @@
+// Package ipinfo provides info on IP address location
+// using the ipinfo.io service.
 package ipinfo
 
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"net/http"
 )
 
+var ipinfoURI = "http://ipinfo.io"
+
+// IPInfo wraps ipinfo.io response
 type IPInfo struct {
-	IP       string
-	Hostname string
-	City     string
-	Country  string
-	Loc      string
-	Org      string
+	IP       string `json:"ip"`
+	Hostname string `json:"hostname"`
+	City     string `json:"city"`
+	Region   string `json:"region"`
+	Country  string `json:"country"`
+	Loc      string `json:"loc"`
+	Org      string `json:"org"`
+	Postal   string `json:"postal"`
 }
 
-//MyIP provides information about your public ip
+// MyIP provides information about the public IP address of the client.
 func MyIP() (*IPInfo, error) {
 	return ForeignIP("")
 }
 
-//ForeignIP provides information about the passed ip
+// ForeignIP provides information about the given IP address,
+// which should be in dotted-quad form.
 func ForeignIP(ip string) (*IPInfo, error) {
 	if ip != "" {
 		ip += "/"
 	}
 
-	response, err := http.Get(fmt.Sprintf("http://ipinfo.io/%sjson", ip))
+	response, err := http.Get(fmt.Sprintf("%s/%sjson", ipinfoURI, ip))
 
 	if err != nil {
 		return nil, err
 	}
-
 	defer response.Body.Close()
-	contents, err := ioutil.ReadAll(response.Body)
-
-	if err != nil {
-		return nil, err
-	}
 
 	var ipinfo IPInfo
-	err = json.Unmarshal(contents, &ipinfo)
-
+	err = json.NewDecoder(response.Body).Decode(&ipinfo)
 	if err != nil {
 		return nil, err
 	}
